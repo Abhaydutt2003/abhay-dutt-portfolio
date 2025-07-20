97562d309e83ba1b2bb4051b4f9ba22e2c997fd0
Abhay Dutt <dutt.abhay@gmail.com>
2025-07-20 06:55:48 +0530

    adds skill badge and work exp section

---
+++ src/components/SkillBadge/index.tsx
@@ -0,0 +1,24 @@
+import "./index.css";
+
+interface SkillBadgeProps {
+  skill: string;
+  size?: "large" | "medium" | "small" | "x-small";
+  className?: string;
+}
+
+const SkillBadge = ({
+  skill,
+  size = "medium",
+  className = "",
+}: SkillBadgeProps) => {
+  return (
+    <span
+      className={`skill-badge skill-badge-size-${size} ${className}`}
+      title={skill}
+    >
+      {skill}
+    </span>
+  );
+};
+
+export default SkillBadge;
--- src/sections/WorkExperience/index.tsx
+++ src/sections/WorkExperience/index.tsx
@@ -1,4 +1,5 @@
-import { Avatar, Divider } from "../../components";
+import { Link } from "lucide-react";
+import { Avatar, Divider, SkillBadge } from "../../components";
 import "./index.css";
 const companies = [
   {
@@ -6,58 +7,94 @@ const companies = [
     time: "July 2024 - April 2025",
     imageUrl: "/gg-logo.jpg",
     role: "Software Engineer Intern",
+    link: "https://res.cloudinary.com/dsihfpv6p/image/upload/Experience_Certificate_GyanGrove_Abhay_boaiqb.pdf",
+    companyLink:
+      "https://www.linkedin.com/company/gyangrove/posts/?feedView=all",
     descripion:
-      "As an early Software Engineer Intern at Gyangrove, I was a founding member who evolved from frontend to full-stack, building a complete admission management CRM and critical features using JavaScript, TypeScript, Node.js, Next.js, and AWS. I also automated testing with Playwright & GitHub Actions and developed AI content generation and role-based management on React Native.",
+      "As an early team member at GyanGrove, I contributed across the stack—starting with frontend (React/Next.js) and growing into full-stack development using Node.js, Express, and AWS. I built core features like an end-to-end admission CRM, school-wide role-based systems, and AI-powered content generation tools in React Native. I also developed CI pipelines integrated with GitHub Actions and BrowserStack to fully automate testing. Constantly adapting to new challenges, I played a key role in shaping the product and engineering practices at the startup. ",
+    skills: [
+      "Typescript",
+      "Javascript",
+      "Python",
+      "React",
+      "Next.js",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "AWS",
+      "React Native",
+      "Expo",
+      "DaisyUi",
+      "GitHub Actions",
+      "BrowserStack",
+      "Playwright",
+      "CI/CD",
+    ],
   },
   {
     name: "Coding Ninjas",
     time: "May 2023 - Sept 2023",
     imageUrl: "/coding-ninjas.png",
     role: "Teaching Assistant",
+    companyLink:
+      "https://www.linkedin.com/school/codingninjas/posts/?feedView=all",
+    link: "https://res.cloudinary.com/dsihfpv6p/image/upload/teaching-assistant-certificate_jtrdgs.pdf",
     descripion:
-      "At Coding Ninjas, I taught 500+ students MERN stack and DSA, achieving a 4.7/5 rating. I focused on explaining complex technical concepts and solving coding challenges, greatly enhancing my technical communication skills.",
+      "Mentored and supported over 500 students in mastering Data Structures & Algorithms and the MERN stack. Conducted doubt-solving sessions, code reviews, and provided personalized guidance throughout their learning journey. Maintained a high student satisfaction rating of 4.7/5, reflecting consistent and impactful teaching.",
+    skills: [
+      "Data Structures",
+      "Algorithms",
+      "MERN Stack",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node.js",
+      "Code Review",
+      "Mentoring",
+    ],
   },
 ];
 
 const WorkExperience = () => {
   return (
     <section className="section-outer-container" id="work-section">
-      <div className="section-left-container section-heading">
+      <div className=" section-left-container section-heading">
         <p>Work Experience</p>
       </div>
       {companies.map((company, index) => {
         return (
           <div className="section-container">
             <div className="section-left-container">
-              <div className="company-logo">
+              <a
+                className="company-logo"
+                href={company.companyLink}
+                title="Click to go to linkdin page"
+              >
                 <Avatar
                   imageUrl={company.imageUrl}
                   alt={`${company.name} logo`}
-                  className="avatar-size-small xs:avatar-size-x-small"
+                  className="avatar-size-small sm:avatar-size-x-small"
                 />
                 <h5>{company.name}</h5>
                 <span>{company.time}</span>
-              </div>
+              </a>
             </div>
             <div className="section-right-container experience-description">
-              <h2>{company.role}</h2>
-              <p>{company.descripion}</p>
-              {/* <a
-                href="https://res.cloudinary.com/dsihfpv6p/image/upload/v1752930236/Experience_Certificate_GyanGrove_Abhay_boaiqb.pdf"
+              <a
+                href={company.link}
                 target="_blank"
                 rel="noopener noreferrer"
+                title="Click to see experience certificate"
               >
-                <img
-                  loading="lazy"
-                  src="https://res.cloudinary.com/dsihfpv6p/image/upload/v1752930236/Experience_Certificate_GyanGrove_Abhay_boaiqb.pdf"
-                  alt="Certificate preview"
-                  style={{
-                    width: "300px",
-                    height: "100px",
-                    borderRadius: "8px",
-                  }}
-                />
-              </a> */}
+                <h2>{company.role}</h2>
+                <Link />
+              </a>
+              <p>{company.descripion}</p>
+              <div className="skills-container">
+                {company.skills?.map((skill, skillIndex) => (
+                  <SkillBadge key={skillIndex} skill={skill} size="x-small" />
+                ))}
+              </div>
               {index < companies.length - 1 && (
                 <Divider noBottomPadding={true} />
               )}
